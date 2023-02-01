9d6b2abc3dabc42beadb1b4be80701c551ac1e1d
iturnmyscreenon <benoit.galey@gmail.com>
2023-02-01 15:03:39 +0100

    responsive navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,19 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import ReorderIcon from "@mui/icons-material/Reorder";
 import logoskonbarb from "../assets/logoskonbarb.png";
 import "../styles/Navbar.css";
 
 export default function Navbar() {
+  const [showLinks, setShowLinks] = useState(false);
+
+  const toggleLinks = () => {
+    setShowLinks(!showLinks);
+  };
+
   return (
     <div className="navbar">
-      <div className="navleft">
-        <img src={logoskonbarb} alt="skonbarb" />{" "}
+      <div className="navleft" id={showLinks ? "open" : "close"}>
+        <img src={logoskonbarb} alt="skonbarb" />
+        <div className="hiddenLinks">
+          <Link to="/"> Home </Link>
+          <Link to="/about"> About </Link>
+          <Link to="/contact"> Contact </Link>
+          <Link to="/gallery"> Gallery </Link>
+        </div>
       </div>
       <div className="navright">
         <Link to="/"> Home </Link>
         <Link to="/about"> About </Link>
         <Link to="/contact"> Contact </Link>
-        <Link to="/gallery"> Gallery </Link>{" "}
+        <Link to="/gallery"> Gallery </Link>
+        <button className="navbtn" type="button" onClick={toggleLinks}>
+          <ReorderIcon />
+        </button>
       </div>
     </div>
   );
