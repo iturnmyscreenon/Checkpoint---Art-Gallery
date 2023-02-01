87bc435ec6b519e4edaadde1d2d7a05121030209
iturnmyscreenon <benoit.galey@gmail.com>
2023-02-01 16:44:14 +0100

    made homepage of the gallery

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import BackGround from "../assets/background.jpg";
+import "../styles/Homepage.css";
 
 export default function Homepage() {
   return (
-    <div>
-      <h1>Homepage</h1>
+    <div className="home">
+      <div
+        className="headerContainer"
+        style={{ backgroundImage: `url(${BackGround})`, height: "100%" }}
+      >
+        <h1>SKONBARB GALLERY</h1>
+        <p>Art Travel</p>
+        <Link to="/gallery">
+          <button type="button">GO TO GALLERY</button>
+        </Link>
+      </div>
     </div>
   );
 }
