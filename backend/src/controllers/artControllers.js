c7e6fdf576f8220cf81d9194380655fa214b0e03
iturnmyscreenon <benoit.galey@gmail.com>
2023-02-01 11:39:13 +0100

    deleted the controlers and route included in the template and replaced with the needed one

---
+++ backend/src/controllers/artControllers.js
@@ -0,0 +1,51 @@
+const models = require("../models");
+
+const browse = (req, res) => {
+  models.item
+    .findAll()
+    .then(([rows]) => {
+      res.send(rows);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+const add = (req, res) => {
+  const item = req.body;
+
+  // TODO validations (length, format...)
+
+  models.item
+    .insert(item)
+    .then(([result]) => {
+      res.location(`/items/${result.insertId}`).sendStatus(201);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+const destroy = (req, res) => {
+  models.item
+    .delete(req.params.id)
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.sendStatus(404);
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+module.exports = {
+  browse,
+  add,
+  destroy,
+};
