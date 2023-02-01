98ae85248ef29f5cdd81850972f9a334a198df07
iturnmyscreenon <benoit.galey@gmail.com>
2023-02-01 19:18:30 +0100

    made artCard propTypes

--- frontend/src/helpers/ArtCard.js
+++ frontend/src/helpers/ArtCard.js
@@ -1,1 +1,11 @@
-import { PropTypes } from "@mui/material";
+import { PropTypes } from "prop-types";
+
+const ArtCard = PropTypes.shape({
+  id: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+  image: PropTypes.shape({ url: PropTypes.string }),
+}).isRequired;
+
+export default ArtCard;
